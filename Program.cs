0e55735249c4ee5a62a51190d664f43ca701a596
Meryem Ebru Uyar <eebru.uyarr@gmail.com>
2025-10-12 16:10:44 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Bir sayı gir: ");
+        int sayi = Convert.ToInt32(Console.ReadLine());
+
+        if (sayi % 2 == 0)
+        {
+            Console.WriteLine($"{sayi} çift bir sayıdır.");
+        }
+        else
+        {
+            Console.WriteLine($"{sayi} tek bir sayıdır.");
+        }
+    }
+}
