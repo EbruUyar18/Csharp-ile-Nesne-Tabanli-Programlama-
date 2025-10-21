0ef5664ac3212cc4966ed2c8334625907c8b91b4
Meryem Ebru Uyar <eebru.uyarr@gmail.com>
2025-10-22 00:33:21 +0300

    Add files via upload

---
+++ startveriable2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Değişken dönüşümleri
+            int x = 10;
+            int y = 6;
+            Console.WriteLine(x + y);
+
+            int a = 1, b = 2, c = 3;
+            int z = a + b + c;
+            Console.WriteLine(z);
+
+            int e, f, g;
+            e = f = g = 2;
+            Console.WriteLine(e + f - g);
+
+        }
+    }
+}
