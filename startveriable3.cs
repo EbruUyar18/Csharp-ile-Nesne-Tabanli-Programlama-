4383d5cda8ef3315fe140d275784bad2bdffec5b
Meryem Ebru Uyar <eebru.uyarr@gmail.com>
2025-10-23 15:18:52 +0300

    Add files via upload

---
+++ startveriable3.cs
@@ -0,0 +1,25 @@
+﻿qusing System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace identifiers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            int 4 bytes
+            long 8 bytes
+            float 4 bytes
+            double 8 bytes
+            bool 1 bytes
+            char 2 bytes
+            string 2 bytes per characters
+            */
+
+        }
+    }
+}
