c1be31aed880587c95fbbc0d48bbb7688151929a
Meryem Ebru Uyar <eebru.uyarr@gmail.com>
2025-10-31 10:06:12 +0300

    Add files via upload

---
+++ whileLOOP.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //LOOP WHİLE
+            int i = 0;
+
+             while (i <5)
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+                //LOOP DO-WHİLE
+            /*
+                do
+                {
+                    Console.WriteLine(i);
+                    i++;
+                }
+                while (i < 5);
+            */
+
+
+            
+        }
+    }
+}
