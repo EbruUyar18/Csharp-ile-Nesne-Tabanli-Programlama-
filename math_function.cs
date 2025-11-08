b081564446a71e56885dd83bcb832c81c2a202bf
Meryem Ebru Uyar <eebru.uyarr@gmail.com>
2025-11-08 23:35:59 +0300

    Add files via upload

---
+++ math_function.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Math.Max(5,10));
+            Console.WriteLine(Math.Min(2,4));
+            Console.WriteLine(Math.Sqrt(81));
+            Console.WriteLine(Math.Abs(Math.Min(-10, -5)));
+            Console.WriteLine(Math.Round(3.3));
+
+        }
+    }
+}
