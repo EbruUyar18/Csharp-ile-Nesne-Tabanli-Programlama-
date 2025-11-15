c6d760e86302b9fc2e8f39b5f51ad747e61f6d96
Meryem Ebru Uyar <eebru.uyarr@gmail.com>
2025-11-15 22:33:15 +0300

    Add files via upload

---
+++ system_linq.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            System.Linq ad alanı
+            min, max gibi yararlı dizi yöntemleri bulunur. 
+            */
+            int[] myNum = { 1, 2, 3, 4, 5, };
+            Console.WriteLine(myNum.Max());
+            Console.WriteLine(myNum.Min());
+            Console.WriteLine(myNum.Sum());
+        }
+    }
+}
