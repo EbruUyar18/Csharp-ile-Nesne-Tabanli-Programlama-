3e3a4f6116017a0028a7622d4d80c5e359ba272e
Meryem Ebru Uyar <eebru.uyarr@gmail.com>
2025-11-15 22:37:05 +0300

    Add files via upload

---
+++ methods.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp15
+{
+    internal class Program
+    {
+        static void myMethod(string fname, int age)
+        {
+            Console.WriteLine(fname + " is " + age +" years old");
+        }
+        static void Main(string[] args)
+        {
+            myMethod("Ebru",20);
+            myMethod("Ahmet", 30);
+            myMethod("zeynep", 22);
+        }
+    }
+}
